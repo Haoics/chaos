582824ad82b3c7b086a05091da59cc1531035cad
Haoics <tomas.ruzza.12@gmail.com>
2021-01-28 22:06:30 +0100

    Loops sistemati

--- 01_ForLoop.R
+++ 01_ForLoop.R
@@ -1,10 +1,10 @@
-link <- vector(mode = "character", length = 46)
+page_link <- vector(mode = "character", length = 46)
 
 for(i in 1:47){
-link[i] <- str_c("https://www.beppegrillo.it/category/archivio/2016/page/",i,"/", sep ="")
+page_link[i] <- str_c("https://www.beppegrillo.it/category/archivio/2016/page/",i,"/", sep ="")
 }
 
-link
+page_link
 
 page <- vector(mode ="list", length = 10)
 sut <-  rep(list(vector(mode ="list", length = 10)), 47)
@@ -13,12 +13,10 @@ page
 sut
 
 for(x in 1:47){
-page<- read_html(link[x]) %>% 
+page<- read_html(page_link[x]) %>% 
   html_nodes(".td_module_10 .td-module-title a") %>% 
   html_attr("href")
   sut[[x]][1:10]<- page
 }
 
-
-
-
+articles <-as_tibble(unlist(sut)) 
