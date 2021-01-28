1bb6ae78e4797fca557afc8ff85aac3ef09b3075
Haoics <tomas.ruzza.12@gmail.com>
2021-01-28 16:13:02 +0100

    Scraping blog

---
+++ 00_Scraping1.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(rvest)
+setwd("/Users/tomasruzza/Documents/Projects/R/Data Access/CE2_thomas_fedra_simo")
+
+url <- "http://www.beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
+browseURL(url)
+
+#robots.txt check
+browseURL("http://www.beppegrillo.it/robots.txt")
+
+#file downloading
+page <- RCurl::getURL(url, 
+                      useragent = str_c(R.version$platform,
+                                        R.version$version.string,
+                                        sep = ", "),
+                      httpheader = c(From = "tomas.ruzza.12@gmail.com"))
+
+#Saving the page
+download.file(url, destfile = "plastica.html")
+
+#Get links
+links <- as_tibble(XML::getHTMLLinks(page))
+
+#try
+
+
