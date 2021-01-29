37a3a058903dd343c893a51f65e74afbace4d72f
Haoics <tomas.ruzza.12@gmail.com>
2021-01-29 17:58:22 +0100

    For loop final

--- 01_ForLoop.R
+++ 01_ForLoop.R
@@ -1,22 +1,32 @@
-page_link <- vector(mode = "character", length = 46)
-
-for(i in 1:47){
-page_link[i] <- str_c("https://www.beppegrillo.it/category/archivio/2016/page/",i,"/", sep ="")
-}
-
-page_link
-
-page <- vector(mode ="list", length = 10)
+link_to_pages <- str_c("https://www.beppegrillo.it/category/archivio/2016/page/", 1:47, "/")
+link_to_pages
 sut <-  rep(list(vector(mode ="list", length = 10)), 47)
-
-page
 sut
 
-for(x in 1:47){
-page<- read_html(page_link[x]) %>% 
-  html_nodes(".td_module_10 .td-module-title a") %>% 
-  html_attr("href")
-  sut[[x]][1:10]<- page
+dir.create("download_beppe")
+
+for (i in seq_along(link_to_pages)) {
+  download.file(url = link_to_pages[i], destfile = here::here("download_beppe", str_c("art", i, ".html")))
+  Sys.sleep(1)
 }
 
-articles <-as_tibble(unlist(sut)) 
+for(x in 1:47){
+  sut[[x]][1:10] <- read_html(here::here("download_beppe", str_c("art", x, ".html"))) %>% 
+  html_nodes(".td_module_10 .td-module-title a") %>% 
+  html_attr("href")
+}
+
+art <- unlist(sut)
+art
+
+out <- rep(list(vector(mode ="list", length = 10)), 47)
+
+for(z in 1:470){
+  out[[z]] <- read_html(art[z]) %>% 
+    html_nodes(".rs_preserve+ div") %>% 
+    html_text()
+}
+
+out
+
+
